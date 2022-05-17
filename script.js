bd08498190210b78e6b374e03c10096a8f96fa83
Rahul Gupta <78619143+rahul5243@users.noreply.github.com>
2022-05-17 16:18:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 const sr = ScrollReveal({
     origin: 'right',
     distance: '200px',
-    duration: 1500,
+    duration: 1000,
     reset: false
 });
 
@@ -41,17 +41,17 @@ sr.reveal('.home__social-icon',{ interval: 200});
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about__subtitle',{delay: 200}); 
+sr.reveal('.about__text',{delay: 200}); 
 
 /*SCROLL QUALIFICATION*/
 sr.reveal('.qualification__tabs',{}); 
-sr.reveal('.qualification__title',{delay: 400});
-sr.reveal('.qualification__subtitle',{delay: 400}); 
-sr.reveal('.qualification__content',{delay: 400}); 
-sr.reveal('.qualification_section',{delay: 400});
-sr.reveal('.section__title',{delay: 400}); 
-sr.reveal('.section__subtitle',{delay: 400}); 
+sr.reveal('.qualification__title',{delay: 200});
+sr.reveal('.qualification__subtitle',{delay: 200}); 
+sr.reveal('.qualification__content',{delay: 200}); 
+sr.reveal('.qualification_section',{delay: 200});
+sr.reveal('.section__title',{delay: 200}); 
+sr.reveal('.section__subtitle',{delay: 200}); 
 
 
 
@@ -59,28 +59,28 @@ sr.reveal('.section__subtitle',{delay: 400});
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
 sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+sr.reveal('.skills__img',{delay: 200});
 
 /*SCROLL Coding Profile*/
 sr.reveal('.coding__subtitle',{}); 
 sr.reveal('.coding__text',{}); 
 sr.reveal('.coding__data',{interval: 200}); 
-sr.reveal('.coding__img',{delay: 600});
+sr.reveal('.coding__img',{delay: 200});
 
 /*SCROLL EXPERIENCE*/
 sr.reveal('.experience__tabs',{}); 
-sr.reveal('.experience__title',{delay: 400});
-sr.reveal('.experience__subtitle',{delay: 400}); 
-sr.reveal('.experience__content',{delay: 400}); 
-sr.reveal('.experience_section',{delay: 400});
-sr.reveal('.section__title',{delay: 400}); 
-sr.reveal('.section__subtitle',{delay: 400}); 
+sr.reveal('.experience__title',{delay: 200});
+sr.reveal('.experience__subtitle',{delay: 200}); 
+sr.reveal('.experience__content',{delay: 200}); 
+sr.reveal('.experience_section',{delay: 200});
+sr.reveal('.section__title',{delay: 200}); 
+sr.reveal('.section__subtitle',{delay: 200}); 
 
 /*SCROLL Project Profile*/
 sr.reveal('.project__subtitle',{}); 
 sr.reveal('.project__text',{}); 
 sr.reveal('.project__data',{interval: 200}); 
-sr.reveal('.project__img',{delay: 600});
+sr.reveal('.project__img',{delay: 200});
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
